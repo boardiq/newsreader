c7b67b4ea49200aa12d1a7ba705efbb9fe4392ae
Adam Carlile <adam@benchmedia.co.uk>
2015-06-08 14:12:42 +0100

    Added truncation

--- lib/newsreader/server/anchorman.rb
+++ lib/newsreader/server/anchorman.rb
@@ -4,6 +4,13 @@ module Newsreader
   module Server
     class Anchorman < Sinatra::Application
 
+      helpers do
+        def truncate(text, length, end_string = ' ...')
+          words = text.split()
+          words = words[0..(length-1)].join(' ') + (words.length > length ? end_string : '')
+        end
+      end
+
       get '/' do
         erb :home, locals: { channels: Newsreader.channels }
       end
